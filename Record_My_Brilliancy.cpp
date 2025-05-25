cfcb918a6f6ce8f53152dd0ddf9ce5d7df55ae64
이병도 <lbd2409@snu.ac.kr>
2025-05-25 22:00:02 +0900

    API기능을 통한 체스판 출력까지 가능 이제 Brilliant PGN기능 구현해야 함

--- Record_My_Brilliancy.cpp
+++ Record_My_Brilliancy.cpp
@@ -2,8 +2,18 @@
 #include <iostream> 
 #include <string>
 #include <vector>
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
 
 using namespace std;
+using json = nlohmann::json;
+
+// 콜백 함수: 서버 응답을 문자열로 저장
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, string* output) {
+    size_t totalSize = size * nmemb;
+    output->append((char*)contents, totalSize);
+    return totalSize;
+}
 
 // moveList decoder
 class MoveDecoder {
@@ -103,15 +113,120 @@ public:
     }
 };
 
+// 입력받은 string으로 작동
+// API에서 게임 정보를 가져오는 클래스
+class ChessFetcher {    
+private:
+    int plyCount;
+    string white, black, result, date, userId, moveList, url;
+
+    // API에서 게임 정보를 가져오는 함수 (월별 게임 정보도 가져옴)
+    void fetchGame() {
+        CURL* curl;
+        CURLcode res;
+        string readBuffer;
+
+        curl = curl_easy_init();
+        if (curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+            curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0");
+
+            res = curl_easy_perform(curl);
+
+            if (res != CURLE_OK) {
+                cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
+            } else {
+                try {
+                    json j = json::parse(readBuffer);
+
+                    plyCount = j["game"]["plyCount"];
+                    white = j["players"]["bottom"]["username"];
+                    black = j["players"]["top"]["username"];
+                    result = j["game"]["resultMessage"];
+                    date = j["game"]["pgnHeaders"]["Date"];
+                    userId = j["players"]["bottom"]["username"];
+                    moveList = j["game"]["moveList"];
+                } catch (json::exception& e) {
+                    cerr << "JSON parsing error: " << e.what() << endl;
+                }
+            }
+
+            curl_easy_cleanup(curl);
+        }
+        return;
+    }
+public:
+    explicit ChessFetcher(const string& _url) : url(_url) {
+        fetchGame();
+    }
+
+    // 여기서 받는 string은 "이 탁월한 수를 보십시오: https://www.chess.com/analysis/game/live/138303590064?move=44&tab=review" 같은 형식
+    // string을 https://www.chess.com/callback/live/game/138303590064 같은 API 형식으로 바꿔주는 함수 (daily, live 구분 필요)
+    static int getBrilliantMoveIndex(const string& s) {
+        size_t pos = s.find("move=");
+        if (pos == string::npos) return -1;
+        pos += 5; // "move=" 길이만큼 이동
+        size_t end = s.find('&', pos);
+        if (end == string::npos) end = s.length(); // '&'가 없으면 문자열 끝까지
+        return stoi(s.substr(pos, end - pos)); // move= 뒤의 숫자를 정수로 변환
+    }
+
+    // daily, live 둘 다 가능
+    static string getGameAPI(const string& s) {
+        size_t start = s.find("live/");
+        if (start != string::npos)  {
+            start += 5; // "live/" 길이만큼 이동
+            size_t end = s.find("?move", start);
+            if (end == string::npos) return "";
+            string gameId = s.substr(start, end - start);
+            return "https://www.chess.com/callback/live/game/" + gameId;
+        } else {
+            start = s.find("daily/");
+            if (start != string::npos) {
+                start += 6; // "daily/" 길이만큼 이동
+                size_t end = s.find("?move", start);
+                if (end == string::npos) return "";
+                string gameId = s.substr(start, end - start);
+                return "https://www.chess.com/callback/daily/game/" + gameId;
+            }
+        }
+        return "";
+    }
+
+    // string을 https://api.chess.com/pub/player/ibottledo/games/2025/05 같은 API 형식으로
+    string getMonthGameAPI() {
+        return "https://api.chess.com/pub/player/" + userId + "/games/" + date.substr(0, 4) + "/" + date.substr(5, 2);
+    }
+
+    string getMoveList() const {
+        return moveList;
+    }
+};
+
 int main() {
     // string moveList = "lBZJcD2Ugv6Lmu5Qks0Sdr45fH9RDw1TblUMeg3NpFMEveRwnwWOfLSLHQXQry7RyO!0et8!tD5jlr0UDURwUDwughuDOQDCagEw";
-
     // int brilliantMoveIndex = 40;
     // ChessBoard chessBoard;
     // // 탁월수 직전의 보드를 출력
     // chessBoard.printChessBoard(moveList, brilliantMoveIndex - 1);
 
+    string Brilliant_url;
+    cout << "Enter: Brilliant URL: ";
+    getline(cin, Brilliant_url);
+    int brilliantMoveIndex = ChessFetcher::getBrilliantMoveIndex(Brilliant_url);
+    string gameAPI = ChessFetcher::getGameAPI(Brilliant_url);
+
+    ChessFetcher fetcher(gameAPI);
+    string moveList = fetcher.getMoveList();
+
+    // cout << "Brilliant Move Index: " << brilliantMoveIndex << endl;
+    // cout << "Game API: " << gameAPI << endl;
+    // cout << "Move List: " << moveList << endl;
+
+    ChessBoard chessBoard;
+    chessBoard.printChessBoard(moveList, brilliantMoveIndex);
+
     return 0;
 }
-
-// 브랜치 확인 주석
