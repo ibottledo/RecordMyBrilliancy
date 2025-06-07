d49e1d6902fb9280b75564763c4297c10cb42ed9
이병도 <lbd2409@snu.ac.kr>
2025-06-07 15:15:30 +0900

    스트릭 아래에 간격 약간

--- generate_streak.cpp
+++ generate_streak.cpp
@@ -64,7 +64,7 @@ int main() {
             << ";display:block;border-radius:3px;'></a>\n";
     }
 
-    out << "</div>\n\n";
+    out << "</div>\n";
 
     cout << "✅ streak.html generated successfully.\n";
     return 0;
--- index.md
+++ index.md
@@ -8,6 +8,8 @@ title: Brilliant Moves
   {% include streak.html %}
 </div>
 
+<div style="margin-top: 16px;"></div>
+
 ---
 
 # [Brilliant Move Archive](https://github.com/ibottledo/RecordMyBrilliancy/tree/main)
