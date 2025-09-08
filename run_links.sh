38a041929c961ab095b1327052f5a3de7ba04004
이병도 <lbd2409@snu.ac.kr>
2025-09-08 17:02:26 +0900

    3연탁

--- run_links.sh
+++ run_links.sh
@@ -7,10 +7,9 @@ DELAY=180
 
 # URL 목록 (원하면 여기에 쭉 추가)
 URLS=(
-    "이 탁월한 수를 보십시오: https://www.chess.com/analysis/game/live/141879128848?move=46&tab=review"
-    "이 탁월한 수를 보십시오: https://www.chess.com/analysis/game/live/141908710966?move=29&tab=review"
-    "이 탁월한 수를 보십시오: https://www.chess.com/analysis/game/live/141976811756?move=12&tab=review"
-    "이 탁월한 수를 보십시오: https://www.chess.com/analysis/game/live/141985809832?move=43&tab=review"
+    "이 탁월한 수를 보십시오: https://www.chess.com/analysis/game/live/142860653076?move=51&tab=review"
+    "이 탁월한 수를 보십시오: https://www.chess.com/analysis/game/live/142860653076?move=59&tab=review"
+    "이 탁월한 수를 보십시오: https://www.chess.com/analysis/game/live/142860653076?move=61&tab=review"
 )
 
 # 반복 실행
